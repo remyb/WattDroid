e58093dd8e1552947820f6ea1609dfd9b3ba44e3
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-04-05 20:21:40 -1000

    fixed classname and respective xml

--- src/edu/hawaii/wattdroid/wattdroid.java
+++ src/edu/hawaii/wattdroid/wattdroid.java
@@ -3,7 +3,7 @@ package edu.hawaii.wattdroid;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class wattdroid extends Activity {
+public class WattDroid extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
