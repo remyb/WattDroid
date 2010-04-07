1a696d6e4f8888aa76bd416af3568b41c876eb02
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-04-06 14:39:26 -1000

    figuring out xml from web service parsing

--- src/edu/hawaii/wattdroid/WattDroid.java
+++ src/edu/hawaii/wattdroid/WattDroid.java
@@ -1,16 +1,25 @@
 package edu.hawaii.wattdroid;
 
-import org.wattdepot.client.WattDepotClient;
-import android.app.ListActivity;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 import android.util.Log;
-import android.view.View;
 
 /**
  * ListView  is a ViewGroup  that creates a list of scrollable items.
@@ -18,80 +27,52 @@ import android.view.View;
  * http://developer.android.com/resources/tutorials/views/hello-listview.html
  * 
  */
-public class WattDroid extends ListActivity {
+public class WattDroid extends Activity {
     /** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
-	  Log.i("wattdroid", "Program Began onCreate()");
-	  //WattDepotClient client = new WattDepotClient("http://server.wattdepot.org:8182/wattdepot/");
-	    /*
-	  	if (client.isHealthy()) {
-	    	//Log.i("WattDepot server found.");
-	    	Log.i("boah", "AAAAAAAAAAAAAAAAAAAAA");
-	    }
-	    else {
-	    	Log.i("boah", "BBBBBBBBBBBBBBBBBBB");
-	    }
-	    */
-	  //automatically adds a ListView  to fill the entire screen of the ListActivity
-	  setListAdapter(new ArrayAdapter<String>(this, R.layout.item, COUNTRIES));
+	  setContentView(R.layout.main);
+	  Log.i("wattdroid", "Started wattdroid");
+	  URL url = null;
+	  try {
+		url = new URL("http://server.wattdepot.org:8182/wattdepot/sources");
+		Log.i("wattdroid", "success!");
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		SAXParser sp = spf.newSAXParser();
+		/* Get the XMLReader of the SAXParser we created. */
+	    XMLReader xr = sp.getXMLReader();
+		
+	    // removed content handler
+	    
+		Log.v("debugging","Hello World");
+		/* Parse the xml-data from our URL */
+		InputStream mystream = url.openStream();
+		InputSource is = new InputSource(mystream);
+		//xr.parse(is);
+		/* Parsing has finished. */
+		Log.i("wattdroid",is.toString());
+		Log.i("wattdroid", "completed");
 
-	  ListView lv = getListView();
-	  lv.setTextFilterEnabled(true);
+	  } catch (MalformedURLException e) {
+		e.printStackTrace();
+	  } catch (ParserConfigurationException e) {
+		e.printStackTrace();
+	  } catch (SAXException e) {
+		e.printStackTrace();
+	  } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  
+      
+      
 
-	  lv.setOnItemClickListener(new OnItemClickListener() {
-	    public void onItemClick(AdapterView<?> parent, View view,
-	        int position, long id) {
-	      // When clicked, show a toast with the TextView text
-	      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
-	          Toast.LENGTH_SHORT).show();
-	    }
-	  });
+	  
+
+
 	}
 	
-	// Stub Data
-	static final String[] COUNTRIES = new String[] {
-	      "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-	      "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-	      "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-	      "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-	      "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-	      "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-	      "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-	      "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-	      "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-	      "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-	      "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-	      "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-	      "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-	      "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-	      "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-	      "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-	      "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-	      "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-	      "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-	      "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-	      "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-	      "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-	      "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-	      "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-	      "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-	      "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-	      "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-	      "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-	      "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-	      "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-	      "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-	      "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-	      "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-	      "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-	      "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-	      "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-	      "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-	      "Ukraine", "United Arab Emirates", "United Kingdom",
-	      "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-	      "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-	      "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
-	    };
+	
+
 }
